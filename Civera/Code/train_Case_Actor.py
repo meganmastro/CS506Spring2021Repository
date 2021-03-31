76ed5a25a240b09017b601d82f1f62a1d52faf67
serra98 <serra.jung@gmail.com>
2021-03-30 20:54:24 -0700

    Update train_Case_Actor.py

--- Civera/Code/train_Case_Actor.py
+++ Civera/Code/train_Case_Actor.py
@@ -16,7 +16,7 @@ from sklearn.metrics import mean_squared_error, confusion_matrix
 
 import mysql.connector
 
-mydb = mysql.connector.connect(host='73.38.248.152', user='buspark', password='U@5p1r3!')
+mydb = mysql.connector.connect(host='', user='', password='')
 
 if (mydb):
     print("Connection Successful")
@@ -29,13 +29,13 @@ mycursor = mydb.cursor()
 case_index_not_null = pd.read_sql("SELECT * FROM wp_courtdocs.cdocs_case_action_index as c_a_index WHERE c_a_index.action != ' ' and c_a_index.actor != ' ' and rand() <= .2;", con = mydb)
 
 columns = ['actor','action','description']
-X_train = case_index_not_null[columns]
-print(X_train.head())
+trainSet = case_index_not_null[columns]
+print(trainSet.head())
 
-# # cdocs_case_action_index / actor = null
-# actor_null = pd.read_sql("SELECT * FROM wp_courtdocs.cdocs_case_action_index as c_a_index WHERE c_a_index.action != ' ' and c_a_index.actor = ' ' and rand() <= .2;", con = mydb)
-# testSet = actor_null[columns]
-# print(testSet.head())
+# cdocs_case_action_index / actor = null
+action_null = pd.read_sql("SELECT * FROM wp_courtdocs.cdocs_case_action_index as c_a_index WHERE c_a_index.action = ' ' and c_a_index.actor != ' ' and rand() <= .2;", con = mydb)
+testSet = action_null[columns]
+print(testSet.head())
 
 # X = trainingSet[['action','description']]
 # y = trainingSet['actor']
@@ -43,27 +43,51 @@ print(X_train.head())
 # X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 42)
 
 r = re.compile(r'[^\w\s]+')
-X_train['description'] = [r.sub('', x) for x in X_train['description'].tolist()]
-X_train['description'] = X_train['description'].str.lower().str.split()
+trainSet['description'] = [r.sub('', x) for x in trainSet['description'].tolist()]
+trainSet['description'] = trainSet['description'].str.lower().str.split()
+testSet['description'] = [r.sub('', x) for x in testSet['description'].tolist()]
+testSet['description'] = testSet['description'].str.lower().str.split()
 
 stopwords = stopwords.words('english')
-X_train['description'] = X_train['description'].apply(lambda x: [item for item in x if item not in stopwords])
+trainSet['description'] = trainSet['description'].apply(lambda x: [item for item in x if item not in stopwords])
 print("stopwords")
-print(X_train.head())
+print(trainSet.head())
+print()
+testSet['description'] = testSet['description'].apply(lambda x: [item for item in x if item not in stopwords])
+print(testSet.head())
+
 
 lemmatizer = WordNetLemmatizer() 
-X_train['description'] = X_train['description'].apply(lambda x:[lemmatizer.lemmatize(word) for word in x])
+trainSet['description'] = trainSet['description'].apply(lambda x:[lemmatizer.lemmatize(word) for word in x])
+testSet['description'] = testSet['description'].apply(lambda x:[lemmatizer.lemmatize(word) for word in x])
 
 #remove duplicate words after lemmatizing 
-X_train['description'] = X_train['description'].apply(lambda x:list(dict.fromkeys(x)))
+trainSet['description'] = trainSet['description'].apply(lambda x:list(dict.fromkeys(x)))
 print()
 print('trainingSet after lemmatizer & removing dupes ')
-print(X_train.head())
-print('processing -remove dupes')
+print(trainSet.head())
 
+testSet['description'] = testSet['description'].apply(lambda x:list(dict.fromkeys(x)))
+print()
+print('testSet after lemmatizer & removing dupes ')
+print(testSet.head())
 
+#copy
+trainSet1 = copy.deepcopy(trainSet)
+testSet1 = copy.deepcopy(testSet)
 
+#join back 
+trainSet1['description'] = trainSet1 ['description'].apply(lambda x:' '.join(x))
+testSet1['description'] = testSet1['description'].apply(lambda x:' '.join(x))
+print()
 
+trainSet1['description'] = trainSet1['description'].astype('str')
+testSet1['description'] = testSet1['description'].astype('str')
+
+#trainSet1.to_csv("./Civera/Data/action_train.csv", mode='w', index = False, header = False)
+#testSet1.to_csv("./Civera/Data/action_test.csv", mode='w', index = False, header = False)
+
+print("done")
 
 
 # def process(df):
